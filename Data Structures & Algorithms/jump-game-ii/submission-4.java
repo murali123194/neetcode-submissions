0ea6565ca3f2708bad8d3fb6cc7615dcf252dd17
murali123194 <muralichadive@gmail.com>
2026-07-26 23:42:23 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l=0,r=0,steps=0;
+        while(r<nums.length-1){
+            int res=l;
+            for(int i=l;i<=r;i++){
+                res=Math.max(res,i+nums[i]);
+            }
+            l=r+1;
+            r=res;
+            steps++;
+
+        }
+        return steps;
+        
+    }
+}
